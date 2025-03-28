a8c2ebaee19808a6cb5778df12debf75507e0310
Temych228 <242098@astanait.edu.kz>
2025-03-28 09:46:37 +0500

    1st task 2.0

--- src/In___This___Task___Function___Find___Minimum___Value___From___Five___Integers___And___Then___Output___It___In___Console___And___You___Grade___This___Task___For___100___From___100.java
+++ src/In___This___Task___Function___Find___Minimum___Value___From___Five___Integers___And___Then___Output___It___In___Console___And___You___Grade___This___Task___For___100___From___100.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class In___This___Task___Function___Find___Minimum___Value___From___Five___Integers___And___Then___Output___It___In___Console___And___You___Grade___This___Task___For___100___From___100 {
     public static int findMin(int[] arr) {
         if (arr == null || arr.length == 0) {
             throw new IllegalArgumentException("Array is empty or null");
